b80af5513af39354f2197e36beec39e93d5bb3e2
Warley Junior <warleyjunior05@gmail.com>
2024-11-21 10:01:12 -0300

    Adicionar dbConnect

--- Backend/API REST/src/app.js
+++ Backend/API REST/src/app.js
@@ -8,8 +8,41 @@ const livros = [
     {id: 2, titulo: 'Poderoso Chefão', autor: 'Mario Puzo'},
 ];
 
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+})
+}
+
 app.get('/', (req, res) => {
     res.status(200).send('Bem vindo ao node.js');
 });
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.get('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+app.post("/livros",(req, res) =>{
+    livros.push(req.body)
+    res.status(201).send("Livro cadastrado com sucesso!")
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso!");
+})
+
 
 export default app;

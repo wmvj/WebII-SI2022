b10265590ce7bfe45be8c4f0130058c36dc4b1a3
wmvj <wmvj@unifesspa.edu.br>
2025-08-26 13:43:26 -0300

    Add mais um exemplo

--- Backend/Typescript/exemplo06.ts
+++ Backend/Typescript/exemplo06.ts
@@ -15,3 +15,20 @@ type SelectResponse = Product[] | null
 function selectProducts(): SelectResponse {
     return null
 }
+
+// Usando o Intersection de tipos
+type Person = {
+    id: number,
+    name: string
+}
+
+type Teacher = Person & {
+    subjects: string[]
+}
+
+type Student = Person & {
+    age: number
+}
+
+let teacher: Teacher
+let student: Student

c561a5a8e08b3b1c8c1e5cbdc63f32a089f37948
wmvj <wmvj@unifesspa.edu.br>
2025-09-09 15:06:08 -0300

    add mudanças

--- Backend/API REST Node Framework/src/database.ts
+++ Backend/API REST Node Framework/src/database.ts
@@ -1,9 +1,16 @@
-import knex from "knex";
+import {knex, Knex} from "knex";
 
-export const setupKnex = knex({
+export const config: Knex.Config ={
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
-  useNullAsDefault:true
-})
+  useNullAsDefault:true,
+  migrations:{
+    extension: 'ts',
+    directory:'./db/migrations'
+  }
+}
+
+
+export const setupKnex = knex(config)
